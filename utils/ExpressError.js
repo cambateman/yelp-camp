d5b071046a39281ef8488bde55a662a22fff8de3
cambateman <cambateman604@gmail.com>
2021-02-04 15:30:26 -0800

    Added review Model, as well as CRUD functionality

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,9 +1,5 @@
-class ExpressError extends Error {
-    constructor(message, statusCode) {
-        super();
-        this.message = message;
-        this.statusCode = statusCode;
+module.exports = func => {
+    return (req, res, next) => {
+        func(req, res, next).catch(next);
     }
 }
-
-module.exports = ExpressError;
