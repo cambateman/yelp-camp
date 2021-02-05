acf6d3b6736cd4a8e38532253e797357d53f5a6c
cambateman <cambateman604@gmail.com>
2021-02-04 16:52:53 -0800

    minor code cleanups

--- app.js
+++ app.js
@@ -117,10 +117,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
-
 app.listen(8080, () => {
     console.log('serving on port 8080')
 })
