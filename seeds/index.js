b897a8e0c728f456eef1b301376326722d94684d
cambateman <cambateman604@gmail.com>
2021-02-06 17:48:35 -0800

    reseeded test db

--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,18 @@ const seedDB = async () => {
             author: '601dabc2d1f8dd0e611fa144',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            images:[
+                {
+                  url: 'https://res.cloudinary.com/damo1rb7w/image/upload/v1612662195/YelpCamp/cwoivqtx5gwtua33zkcn.jpg',
+                  filename: 'YelpCamp/cwoivqtx5gwtua33zkcn'
+                },
+                {
+                  url: 'https://res.cloudinary.com/damo1rb7w/image/upload/v1612662195/YelpCamp/ro3mhrxuhs8b3gzgiwkl.jpg',
+                  filename: 'YelpCamp/ro3mhrxuhs8b3gzgiwkl'
+                }
+              ]
         })
         await camp.save();
     }
